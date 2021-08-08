f2b8931ddeec799d7c64f8c77c12a8678d2f58e4
tgthuan <tgthuan2000@gmail.com>
2021-08-08 17:18:42 +0700

    commit

--- src/features/ToeicNovel/pages/exam/randomContent.js
+++ src/features/ToeicNovel/pages/exam/randomContent.js
@@ -1,17 +1,24 @@
+import {KEY_EXAM_WORD} from "../../../../localStorageContans"
+
 const randomContent = (words, trans) => {
   //   console.log(words, trans);
-  const index = Math.floor(Math.random() * words.length);
+  const data = JSON.parse(localStorage.getItem(KEY_EXAM_WORD)) || ""
+  let index = -1
+  do{
+	index = Math.floor(Math.random() * words.length);
+  } while(words[index] === data);
+  localStorage.setItem(KEY_EXAM_WORD, JSON.stringify(words[index]))
   //   console.log(index);
   return {
     word: words[index],
     correct: trans[index],
-    answers: mixAnswer(trans[index], getIncorrectAnswer(1, trans, index)),
+    answers: mixAnswer(trans[index], getIncorrectAnswer(trans, index, 5)),
   };
 };
 
 export default randomContent;
 
-const getIncorrectAnswer = (number = 1, trans, index) => {
+const getIncorrectAnswer = (trans, index, number = 1) => {
   let result = [];
   let newTrans = [...trans];
   newTrans.splice(index, 1);
--- src/localStorageContans.js
+++ src/localStorageContans.js
@@ -1,2 +1,3 @@
 export const KEY_NOTE = "dictionary-data-note";
 export const KEY_TRANSLATE = "dictionary-data-translate";
+export const KEY_EXAM_WORD = "dictionary-data-word"
